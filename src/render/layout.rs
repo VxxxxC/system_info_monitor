30fd590d9c71395bc0b2179f6225d550b6e96754
vxxxxc <veper1234@icloud.com>
2025-08-27 01:14:59 +0800

    [WIP] display location still not fix for unalignment

--- src/render/layout.rs
+++ src/render/layout.rs
@@ -2,7 +2,7 @@ use crate::system::os_info;
 use crate::system::os_info::*;
 use ratatui::layout::{Constraint, Direction, Flex, Layout, Rect};
 use ratatui::prelude::*;
-use ratatui::widgets::BorderType;
+use ratatui::widgets::{BorderType, Padding, Wrap};
 use ratatui::{
     DefaultTerminal, Frame,
     widgets::{Block, Borders, Paragraph},
@@ -14,18 +14,18 @@ pub fn main_layout(frame: &mut Frame) {
         .constraints(vec![Constraint::Percentage(30), Constraint::Percentage(20)])
         .split(frame.area());
 
-    let os_info_block = Block::bordered()
+    let os_info_block = Block::default()
         .fg(Color::Blue)
         .border_type(BorderType::Rounded)
         .borders(Borders::ALL)
-        .title(Line::from("System Info").centered());
+        .title(Span::from("System Info").bold().into_centered_line());
 
     let inner_block = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)])
+        .constraints(vec![Constraint::Percentage(50)]).flex(Flex::Center)
         .split(container[0]);
 
-    let center = center_container(frame.area(), Constraint::Length(1), Constraint::Length(1));
+    frame.render_widget(os_info_block, inner_block[0]);
 
     let mut info_list = os_info::system_info();
     let mut title: Vec<String> = Vec::new();
@@ -35,18 +35,17 @@ pub fn main_layout(frame: &mut Frame) {
         title.push(x.title.clone());
         info.push(x.info.clone());
     }
+    const LENGTH: usize = 6;
 
-    frame.render_widget(os_info_block, container[0]);
-
-    let title_layout = Layout::vertical([Constraint::Length(20)]).split(inner_block[0]);
-    let info_layout = Layout::vertical([Constraint::Length(20)]).split(inner_block[1]);
+    let title_layout = Layout::vertical([Constraint::Length(5); LENGTH]).split(inner_block[0]);
+    let info_layout = Layout::vertical([Constraint::Length(5); LENGTH]).split(inner_block[0]);
 
     for range in 0..info_list.len() {
         if range <= info_list.len() {
-            let title = Text::from(Line::from(title[range].clone()));
-            let info = Text::from(Line::from(info[range].clone()));
-            frame.render_widget( Paragraph::new(title).alignment(Alignment::Left), title_layout[0]);
-            frame.render_widget( Paragraph::new(info).alignment(Alignment::Right), info_layout[0]);
+            let title = Line::from(title[range].clone());
+            let info = Line::from(info[range].clone());
+            frame.render_widget( Paragraph::new(title).alignment(Alignment::Left), title_layout[range]);
+            frame.render_widget( Paragraph::new(info).alignment(Alignment::Right), info_layout[range]);
         }
     }
 
